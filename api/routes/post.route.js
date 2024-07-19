b1a29aa538dfc0a304a3ac15f2daa5ee9c290973
Sai Pone Kha Aung <saiponekhaaaung@gmail.com>
2024-07-19 09:22:25 +0700

    Api chat, message, post, user

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,18 +1,13 @@
 import express from 'express';
+import { verifyToken } from '../middleware/verifyToken.js';
+import { getPosts, getPost, addPost, updatePost, deletePost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
-// router.get("/test", (req, res) => {
-//     res.send("It's post!");
-// })
-// router.post("/test", (req, res) => {
-//     res.send("It's post!");
-// })
-// router.put("/test", (req, res) => {
-//     res.send("It's post!");
-// })
-// router.delete("/test", (req, res) => {
-//     res.send("It's post!");
-// })
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", verifyToken, addPost);
+router.put("/:id", verifyToken, updatePost);
+router.delete("/:id", verifyToken,deletePost);
 
 export default router;
